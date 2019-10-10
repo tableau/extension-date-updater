98c93bb9a0a0da7809e6a46651985d837be9f0ff
krose <krose@tableau.com>
2019-10-10 12:38:19 -0700

    Added source for sandboxed version

---
+++ sandboxed/src/entry_config.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Configure from './Configure';
+
+ReactDOM.render(<Configure />, document.getElementById('container'));
